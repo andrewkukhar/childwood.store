dda237bec2c62287adec49ef846d1345334caa0f
andreyrise <andrey.rise@gmail.com>
2021-11-30 14:04:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,51 @@
-// "use strict"
+/* Прокручивает страницу вверх при нажатии на кнопку */
+$(function () {
+    // при нажатии на кнопку scrollup
+    $('.scrollup').click(function () {
+        // переместиться в верхнюю часть страницы
+        $("html, body").animate({
+            scrollTop: 0
+        }, 300);
+    })
+})
+// при прокрутке окна (window)
+$(window).scroll(function () {
+    // если пользователь прокрутил страницу более чем на 200px
+    if ($(this).scrollTop() > 400) {
+        // то сделать кнопку scrollup видимой
+        $('.scrollup').fadeIn();
+    }
+    // иначе скрыть кнопку scrollup
+    else {
+        $('.scrollup').fadeOut();
+    }
+});
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     const form = document.getElementById('form');
-//     form.addEventListener('submit', formSend);
-
-//     async function formSend(e) {
-//         e.preventDefault();
-
-//         // let error = formValidate(form);
-
-//         let formData = new FormData(form);
-
-//         if (error === 0) {
-//             form.classList.add('_sending');
-//             let response = await fetch('sendmail.php', {
-//                 method: 'POST',
-//                 body: formData
-//             });
-//             if (response.ok) {
-//                 let result = await response.json();
-//                 alert(result.message);
-//                 form.reset();
-//                 form.classList.remove('_sending');
-//             } else {
-//                 alert("Помилка");
-//                 form.classList.remove('_sending');
-//             }
-//         } else {
-//             alert('Заповніть всі поля');
-//         }
-//     }
-//     // function formValidate(form) {
-//     //     let error = 0;
-//     //     let formReq = document.querySelectorAll('._req');
-//     //     for (let index = 0; index < formReq.length; index++) {
-//     //         const input = formReq[index];
-//     //         formRemoveError(input);
-
-//     //         if (input.value === '') {
-//     //             formAddError(input);
-//     //             error++;
-//     //         }
-//     //     }
-//     //     return error;
-//     // }
-//     // function formAddError(input) {
-//     //     input.parentElement.classList.add('_error');
-//     //     input.classList.add('_error');
-//     // }
-//     // function formRemoveError(input) {
-//     //     input.parentElement.classList.remove('_error');
-//     //     input.classList.remove('_error');
-//     // }
-// });
+$(document).ready(function () {
+    $('.slider').slick({
+      arrows: true,
+      dots: true,
+      adaptiveHeight: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      speed: 700,
+      easing: 'linear',
+      infinite: true,
+      initialSlide: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      pauseOnFocus: true,
+      pauseOnHover: true,
+      pauseOnDotsHover: true,
+      rows: 1,
+      slidesPerRows: 1,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ],
+    });
+  });
