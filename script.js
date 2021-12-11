d61914a3191b878a29ecd074dc0d43c49c84360d
andreyrise <andrey.rise@gmail.com>
2021-12-11 20:15:59 +0200

    add style font

--- script.js
+++ script.js
@@ -49,15 +49,3 @@ $(document).ready(function () {
     ],
   });
 });
-
-var fileinclude = require('gulp-file-include'),
-  gulp = require('gulp');
-
-gulp.task('fileinclude', function () {
-  gulp.src(['index.html'])
-    .pipe(fileinclude({
-      prefix: '@@',
-      basepath: '@file'
-    }))
-    .pipe(gulp.dest('./'));
-});
