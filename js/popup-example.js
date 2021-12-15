04a765c7509ee75898c999d5d17d14330f637992
andreyrise <andrey.rise@gmail.com>
2021-12-15 11:39:50 +0200

    add style for popup-form's

--- js/popup-example.js
+++ js/popup-example.js
@@ -1,8 +1,9 @@
-const bedForm = document.forms.bedforms;
-const bedFormModel = bedForm.model;
-// const bedFormModelSelect = bedFormModel.selectedIndex;
-// console.log(bedFormModelSelect);
-bedFormModel.addEventListener("change", function (e) {
-    let index = bedFormModel.selectedIndex;
-    console.log(index);
+let bedForm = document.getElementById('model');
+let photoItem = document.querySelectorAll('.popup-sideofbed-example-content');
+let lastIndex = 0;
+bedForm.addEventListener("change", function () {
+    photoItem[lastIndex].style.display = "none";
+    let indexSel = bedForm.selectedIndex;
+    photoItem[indexSel].style.display = "block";
+    lastIndex = indexSel;
 });
