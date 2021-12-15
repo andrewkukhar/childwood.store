90e3dcdf1ae3bfea588c3bd1a8396ce3582b791b
andreyrise <andrey.rise@gmail.com>
2021-12-15 09:25:27 +0200

    add style for popup-form's

---
+++ js/popup-example.js
@@ -0,0 +1,12 @@
+// document.forms
+// console.log(document.forms);
+
+const bedForm = document.getElementById('formb');
+console.log(bedForm);
+
+const bedFormModel = bedForm.model;
+const bedFormModelValue = bedFormModel.value;
+console.log(bedFormModelValue);
+
+const bedModel = document.querySelectorAll('.bed-model');
+console.log(bedModel);
