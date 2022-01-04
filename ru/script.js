6028146fbe96d804d4b787357b4bed931396ded5
andreyrise <andrey.rise@gmail.com>
2022-01-04 10:54:49 +0200

    update repository

--- ru/script.js
+++ ru/script.js
@@ -1,44 +1,45 @@
 $(function () {
-    $('.scrollup').click(function () {
-        $("html, body").animate({
-            scrollTop: 0
-        }, 300);
-    })
+  $('.scrollup').click(function () {
+    $("html, body").animate({
+      scrollTop: 0
+    }, 300);
+  })
 })
 $(window).scroll(function () {
-    if ($(this).scrollTop() > 400) {
-        $('.scrollup').fadeIn();
-    }
-    else {
-        $('.scrollup').fadeOut();
-    }
+  if ($(this).scrollTop() > 400) {
+    $('.scrollup').fadeIn();
+  }
+  else {
+    $('.scrollup').fadeOut();
+  }
 });
 
 $(document).ready(function () {
-    $('.slider').slick({
-      arrows: true,
-      dots: true,
-      adaptiveHeight: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      speed: 700,
-      easing: 'linear',
-      infinite: true,
-      initialSlide: 1,
-      autoplay: true,
-      autoplaySpeed: 1500,
-      pauseOnFocus: true,
-      pauseOnHover: true,
-      pauseOnDotsHover: true,
-      rows: 1,
-      slidesPerRows: 1,
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 1
-          }
+  $('.slider').slick({
+    arrows: true,
+    dots: true,
+    adaptiveHeight: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 700,
+    easing: 'linear',
+    infinite: true,
+    initialSlide: 0,
+    autoplay: false,
+    autoplaySpeed: 1500,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+    rows: 1,
+    slidesPerRows: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          autoplay: true
         }
-      ],
-    });
+      }
+    ],
   });
+});
